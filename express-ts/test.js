193d3d0a5e6723110df0a342ecd9161f3d8e6445
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-10-02 10:07:42 +0700

    chore: init docker configuration

--- express-ts/test.js
+++ express-ts/test.js
@@ -1,18 +1,18 @@
-import http from 'k6/http';
-import { sleep,check } from 'k6';
+import { check, sleep } from "k6";
+import http from "k6/http";
 
 export const options = {
-	vus: process.env.K6_VUS,
-	duration: process.env.K6_DURATION,
-	  cloud: {
-		      projectID: process.env.K6_PROJECT_ID,
-		        }
-		        }
+  vus: process.env.K6_VUS,
+  duration: process.env.K6_DURATION,
+  cloud: {
+    projectID: process.env.K6_PROJECT_ID,
+  },
+};
 
-export default function (){
- const res = http.get(`${process.env.K6_API_URL}/api/all`);
-	  check(res, {
-		      "status was 200": (r) => r.status == 200,
-		    });
-	  sleep(1);
+export default function () {
+  const res = http.get(`${process.env.K6_API_URL}/api/all`);
+  check(res, {
+    "status was 200": (r) => r.status == 200,
+  });
+  sleep(1);
 }
--- golang/test.js
+++ golang/test.js
@@ -1,18 +1,25 @@
-import http from 'k6/http';
-import { sleep,check } from 'k6';
+import { check, sleep } from "k6";
+import http from "k6/http";
+
+const env = {
+  vus: process.env.K6_VUS,
+  duration: process.env.K6_DURATION,
+  projectID: process.env.K6_PROJECT_ID,
+  apiUrl: process.env.K6_API_URL,
+};
 
 export const options = {
-	vus: process.env.K6_VUS,
-	duration: process.env.K6_DURATION,
-	  cloud: {
-		      projectID: process.env.K6_PROJECT_ID,
-		        }
-		        }
+  vus: env.vus,
+  duration: env.duration,
+  cloud: {
+    projectID: env.projectID,
+  },
+};
 
-export default function (){
- const res = http.get(`${process.env.K6_API_URL}/api/all`);
-	  check(res, {
-		      "status was 200": (r) => r.status == 200,
-		    });
-	  sleep(1);
+export default function () {
+  const res = http.get(`${env.apiUrl}/api/all`);
+  check(res, {
+    "status was 200": (r) => r.status == 200,
+  });
+  sleep(1);
 }
